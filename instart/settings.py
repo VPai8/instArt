440525ca4989d753a98bc022044e8f423451bffb
vpai <vighnesh.pai.8@gmail.com>
2020-12-16 02:21:03 +0530

    Updated settings

--- instart/settings.py
+++ instart/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#qly8bfo53r)jkl=w&g_(w=1-q26g2h)qmul=f#e5i5s+_(sqp'
+# SECRET_KEY = '#qly8bfo53r)jkl=w&g_(w=1-q26g2h)qmul=f#e5i5s+_(sqp'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
